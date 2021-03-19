6da8c308fe2221614026f558d2aaabdcafa966c6
Vishwanath Ezhil <54872169+vishwa742@users.noreply.github.com>
2021-03-19 01:39:10 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  //const colors = ["red", "blue", "green", "yellow", "black"];
+  const color = "red";
+  const [modal, setModal] = useState("");
+  const [count, setCount] = useState("");
+
+  const [value, setValue] = useState("purple");
+  const redClick = () => {
+    setValue("red");
+    setModal("");
+    setCount("");
+  };
+  const greenClick = () => {
+    setValue("green");
+    setModal("");
+    setCount("");
+  };
+  const yellowClick = () => {
+    setValue("yellow");
+    setModal("");
+    setCount("");
+  };
+  const blueClick = () => {
+    setValue("blue");
+    setModal("");
+    setCount("");
+  };
+  var clicks = 0;
+
+  function countClick() {
+    clicks += 1;
+    console.log(value, clicks);
+  }
+
+  function submitCount() {
+    setModal(clicks);
+    setCount(value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h2>Button and Colors</h2>
+        <h1>{value}</h1>
+        <h1>
+          {count} {modal}
+        </h1>
+
+        <button className="btn" onClick={redClick}>
+          Red
+        </button>
+        <button className="btn" onClick={greenClick}>
+          Green
+        </button>
+        <button className="btn" onClick={yellowClick}>
+          Yellow
+        </button>
+        <button className="btn" onClick={blueClick}>
+          Blue
+        </button>
+        <button className="btn" onClick={submitCount}>
+          Submit
+        </button>
+      </div>
+      <div class="grid" style={{ backgroundColor: value }} onClick={countClick}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
   );
 }
 
