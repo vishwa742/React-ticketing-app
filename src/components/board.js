95aba05a3b82a2da9123a7e519c318626838e468
Vishwanath Ezhil <54872169+vishwa742@users.noreply.github.com>
2021-03-23 21:38:24 -0400

    updated board

--- src/components/board.js
+++ src/components/board.js
@@ -8,43 +8,105 @@ const Board = () => {
   const [board, setBoard] = useState(
     new Array(BOARD_SIZE).fill(0).map((row) => new Array(BOARD_SIZE).fill(0))
   );
-  const displayPos = (cell) => {
-    console.log(cell);
+
+  const [rowLoc, setRowLoc] = useState([]);
+  const [colLoc, setColLoc] = useState([]);
+  const [user1, setUser1] = useState(["black"]);
+
+  const getCellClassName = (user1) => {
+    let className = "cell";
+    console.log(className);
+
+    return className;
+  };
+
+  const displayPos = (rowIdx, cellIdx) => {
+    console.log(rowIdx, cellIdx);
+    setRowLoc(rowIdx);
+    setColLoc(cellIdx);
+    setBoard(
+      new Array(rowIdx + 1).fill(0).map((row) => new Array(cellIdx + 1).fill(0))
+    );
   };
 
   return (
-    <div className="board">
-      {board.map((row, rowIdx) => (
-        <div key={rowIdx} className="row">
-          {row.map((cell, cellIdx) => (
-            <div
-              key={cellIdx}
-              className="cell"
-              onClick={() => {
-                console.log(rowIdx, cellIdx);
-              }}
-            ></div>
-          ))}
-        </div>
-      ))}
-      <div>
+    <>
+      <button
+        className="btn"
+        key="1"
+        onClick={() => {
+          setUser1("red");
+        }}
+      >
+        User 1 :red
+      </button>
+      <button
+        className="btn"
+        key="2"
+        onClick={() => {
+          setUser1("green");
+        }}
+      >
+        User 2 : green
+      </button>
+      <button
+        className="btn"
+        key="3"
+        onClick={() => {
+          setUser1("blue");
+        }}
+      >
+        User 3 : blue
+      </button>
+      <button
+        className="btn"
+        key="4"
+        onClick={() => {
+          setUser1("purple");
+        }}
+      >
+        User 4 : purple
+      </button>
+
+      <p>{user1}</p>
+
+      <div className="board">
         {board.map((row, rowIdx) => (
-          <div key={rowIdx}>
-            {row.map((cellValue, cellIdx) => (
-              <div key={cellIdx} className="item">
-                {rowIdx}, {cellIdx}
-              </div>
+          <div key={rowIdx} className="row">
+            {row.map((cell, cellIdx) => (
+              <div
+                key={cellIdx}
+                className="cell"
+                onClick={() => displayPos(rowIdx, cellIdx)}
+              ></div>
             ))}
-            {/* const className = getCellClassName(cellValue, row,);
-              return (
-                <div key={cellIdx} className="item">
-                  {row} {"test"}
-                </div>
-              ); */}
           </div>
         ))}
       </div>
-    </div>
+
+      <div className="item">
+        {rowLoc},{colLoc}
+      </div>
+    </>
+
+    // <div className="board">
+    //     {board.map((row, rowIdx) => (
+    //       <div key={rowIdx} className="row">
+    //         {row.map((cell, cellIdx) => (
+    //           <div
+    //             key={cellIdx}
+    //             className="cell"
+    //             onClick={() => displayPos(rowIdx, cellIdx)}
+    //           ></div>
+    //         ))}
+    //       </div>
+    //     ))}
+    //     <div className="item">
+    //       {" "}
+    //       {rowLoc},{colLoc}
+    //     </div>
+    //   </div>
+    // </>
   );
 };
 
@@ -62,3 +124,9 @@ export default Board;
 //     </div>
 //   );
 // };
+
+// onClick={() => {
+//                   console.log(typeof board);
+//                   return board[rowIdx][cellIdx];
+//                   displayPos(rowIdx, cellIdx);
+//                 }}
