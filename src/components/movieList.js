ac765632353c211c3ca433eb05434db94315f21c
Vishwanath Ezhil <54872169+vishwa742@users.noreply.github.com>
2021-04-02 23:26:11 -0400

    moved the movie api call to app.js

--- src/components/movieList.js
+++ src/components/movieList.js
@@ -1,38 +1,12 @@
 import React, { useState } from "react";
 import "./board.css";
 
-const API_KEY = "a2ccb0a6c2116c753ac57cfe0bb5b417";
-const url =
-  "https://api.themoviedb.org/3/movie/now_playing?api_key=a2ccb0a6c2116c753ac57cfe0bb5b417";
-
 function MovieList() {
-  const [movieNames, setMovieNames] = useState([]);
-  const list = [];
-  const getResults = () => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        const results = data.results;
-
-        results.forEach((movie) => {
-          list.push(movie.title);
-        });
-        setMovieNames(list);
-        console.log(typeof movieNames);
-
-        // console.log("Data:", results[0]);
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-      });
-    //return list;
-  };
-
   return (
-    <article className="movies">
-      <button onClick={getResults}>Click me</button>
+    <article className="movie">
       <img
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.6qpVJwvjH9StXx-bBtICwgHaK9%26pid%3DApi&f=1"
+        width="300px"
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Ep9HA3VwoEb_2NcGuBzhrQHaK9%26pid%3DApi&f=1"
         alt=""
       />
       <h3>Captain Marvel</h3>
